12c6dc70239c8e157eed2017bb4c834a68433a9a
Advik <advik.singh@one2n.in>
2026-04-13 14:23:28 +0530

    fix: update ssh keys

--- modules/users.nix
+++ modules/users.nix
@@ -7,7 +7,10 @@
     extraGroups = [ "wheel" "docker" "networkmanager" ];
     shell = pkgs.nushell;
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILW+fYDg8dBl4plpLl1CkQ9iHkx3BJi1l6V1XI4DV3n1"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPfBhL4psTGBlo+emwtTPdaySzaoScsbiKl4Z1tx1SXF"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMc4CmyjdX1qaResQVgFLipk2hK1YiUpCI1dJROIoHEP"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMV6lTfN9wnRCQEMrRmBRGMN6uRXQv+tCYU33KXeVTXW"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOyg+AejaQa5XoFMJnvWaQMmGR2YH9U3v1Tgw0oSQU6c"
     ];
   };
 
